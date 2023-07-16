2583fa54fc66451a7fbe26628b86c79749a17bf7
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2023-07-16 05:31:44 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -3,7 +3,7 @@
 ##
 ##	RockPro64 SPI-Uboot Assembler
 ##		Requirements: debian based OS already running on the RockPro64, any size Fat formatted microsd in the MMCBLK1 slot w/ no MBR/GUID
-##    By: Shant Tchatalbachian
+##		  By: Shant Tchatalbachian
 ##
 
 cd /tmp/
