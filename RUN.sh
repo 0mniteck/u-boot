3e7eb37af6f1c4ceb66cd1a3cddcd4fd1f009f59
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2023-09-25 14:03:58 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
 ##
-##	RockPro64 SPI-Uboot Assembler
-##		Requirements: debian based OS already running on the RockPro64, any size Fat formatted microsd in the MMCBLK1 slot w/ no MBR/GUID
+##	PinePhone SPI-Uboot Assembler
+##		Requirements: Debian based OS running on ARM64, any size Fat formatted microsd in the MMCBLK1 slot w/ no MBR/GUID
 ##		  By: Shant Tchatalbachian
 ##
 
-cd /tmp/
+pushd /tmp/
 apt update && apt install build-essential bc zip unzip bison flex libssl-dev gcc-arm-none-eabi device-tree-compiler swig python3-pyelftools python3-dev -y
 wget https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/v2.9.zip
 echo '07e2500d5a64d4ebce5e5d7a934bdb4e911457402a84a8ca0070e42a65fe424596bb0995d03122867e08d459933f45eb7dd5478a5fcccd03afd16625e0dc2d3d  v2.9.zip' > v2.zip.sum
@@ -19,16 +19,15 @@ unzip v2.*.zip
 cd arm-trusted-firmware-*
 sed -i '/--fatal-warnings -O1/ s/$/ --no-warn-rwx-segments/' Makefile
 make realclean
-make PLAT=rk3399
-export BL31=/tmp/arm-trusted-firmware-2.9/build/rk3399/release/bl31/bl31.elf
+make PLAT=sun50i_a64
+export BL31=/tmp/arm-trusted-firmware-2.9/build/sun50i_a64/release/bl31/bl31.elf
 cd ..
 cd u-boot-202*
-sed -i 's/CONFIG_BAUDRATE=1500000/CONFIG_BAUDRATE=115200/' configs/rockpro64-rk3399_defconfig
-make rockpro64-rk3399_defconfig
+make pinephone_defconfig
 make all
 image_name="spi_idbloader.img"
 combined_name="spi_combined.img"
-tools/mkimage -n rk3399 -T rkspi -d tpl/u-boot-tpl.bin:spl/u-boot-spl.bin "${image_name}"
+tools/mkimage -n pinephone -T rkspi -d tpl/u-boot-tpl.bin:spl/u-boot-spl.bin "${image_name}"
 padsize=$((0x60000 - 1))
 image_size=$(wc -c < "${image_name}")
 [ $image_size -le $padsize ] || exit 1
@@ -37,8 +36,14 @@ cat ${image_name} u-boot.itb > "${combined_name}"
 mount /dev/mmcblk1 /mnt
 sha512sum spi_combined.img
 sha512sum spi_combined.img > /mnt/spi_combined.img.sum
+sha512sum spi_combined.img > /tmp/spi_combined.img.sum
 cp spi_combined.img /mnt/spi_combined.img
+cp spi_combined.img /tmp/spi_combined.img
 sync
 umount /mnt
-cd ..
+popd
 apt remove --purge build-essential bc zip unzip bison flex libssl-dev gcc-arm-none-eabi device-tree-compiler swig python3-pyelftools python3-dev -y && apt autoremove -y && rm -f -r /tmp/u-boot-202* && rm -f /tmp/lts-* && rm -f /tmp/v2* && rm -f -r /tmp/arm-trusted-firmware-*
+rm spi_combined.zip && zip -0 spi_combined.zip /tmp/spi_combined.img /tmp/spi_combined.img.sum
+git status
+git add -A
+git commit -a -S -m "Successful Build of U-Boot with TF-A"
