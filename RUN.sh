334e54bee8cd340dafbc25b9a6073f2802f1daa2
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2023-09-25 17:18:31 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -7,7 +7,7 @@
 ##
 
 pushd /tmp/
-apt update && apt install build-essential bc zip unzip bison flex libssl-dev gcc-riscv64-linux-gnu gcc-arm-none-eabi gcc-or1k-elf binutils-or1k-elf libgcc-s1-riscv64-cross device-tree-compiler swig python3-pyelftools python3-dev -y
+apt update && apt install build-essential bc zip unzip bison flex libssl-dev gcc-arm-none-eabi gcc-or1k-elf binutils-or1k-elf libgcc-s1-riscv64-cross device-tree-compiler swig python3-pyelftools python3-dev -y
 wget https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/v2.9.zip
 echo '07e2500d5a64d4ebce5e5d7a934bdb4e911457402a84a8ca0070e42a65fe424596bb0995d03122867e08d459933f45eb7dd5478a5fcccd03afd16625e0dc2d3d  v2.9.zip' > v2.zip.sum
 if [[ $(sha512sum -c v2.zip.sum) == 'v2.9.zip: OK' ]]; then sleep 0; else exit 1; fi;
@@ -27,7 +27,7 @@ make PLAT=sun50i_a64 bl31
 export BL31=/tmp/arm-trusted-firmware-2.9/build/sun50i_a64/release/bl31/bl31.elf
 cd ..
 cd crust-0.*
-export CROSS_COMPILE=or1k-linux-musl-
+export CROSS_COMPILE=or1k-elf-
 make pinephone_defconfig && make -j5 scp
 export SCP=/src/crust/build/scp/scp.bin
 // export SCP=/dev/null
@@ -53,7 +53,7 @@ cp spi_combined.img /tmp/spi_combined.img
 sync
 umount /mnt
 popd
-apt remove --purge build-essential bc zip unzip bison flex libssl-dev gcc-riscv64-linux-gnu gcc-arm-none-eabi gcc-or1k-elf binutils-or1k-elf device-tree-compiler swig python3-pyelftools python3-dev -y && apt autoremove -y && rm -f -r /tmp/u-boot-202* && rm -f /tmp/lts-* && rm -f /tmp/v2* && rm -f /tmp/v0* && rm -f -r /tmp/crust-0.* && rm -f -r /tmp/arm-trusted-firmware-*
+apt remove --purge build-essential bc zip unzip bison flex libssl-dev gcc-arm-none-eabi gcc-or1k-elf binutils-or1k-elf device-tree-compiler swig python3-pyelftools python3-dev -y && apt autoremove -y && rm -f -r /tmp/u-boot-202* && rm -f /tmp/lts-* && rm -f /tmp/v2* && rm -f /tmp/v0* && rm -f -r /tmp/crust-0.* && rm -f -r /tmp/arm-trusted-firmware-*
 rm spi_combined.zip && zip -0 spi_combined.zip /tmp/spi_combined.img /tmp/spi_combined.img.sum
 git status
 git add -A
