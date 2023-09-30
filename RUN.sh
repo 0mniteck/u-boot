ca2492f4bb81c0dfb585ca32ac3becfbc7fddf76
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2023-09-30 10:09:37 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -7,11 +7,10 @@
 ##
 
 git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
+rm -f build.zip
 pushd /tmp/
 apt update && apt install build-essential bc zip unzip bison flex libssl-dev gcc-arm-linux-gnueabi gcc-or1k-elf binutils-or1k-elf device-tree-compiler swig python3-pyelftools python3-dev -y
-wget https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/v2.9.zip
-echo '07e2500d5a64d4ebce5e5d7a934bdb4e911457402a84a8ca0070e42a65fe424596bb0995d03122867e08d459933f45eb7dd5478a5fcccd03afd16625e0dc2d3d  v2.9.zip' > v2.zip.sum
-if [[ $(sha512sum -c v2.zip.sum) == 'v2.9.zip: OK' ]]; then sleep 0; else exit 1; fi;
+git clone git@Arm:0mniteck/arm-trusted-firmware.git
 wget https://github.com/u-boot/u-boot/archive/refs/tags/v2023.07.02.zip
 echo '3293f165ea9b381d4c1e86a40585a9e5b242da2a37f19b592e23983c9a92ba76a3e4c9b8c56dfd4faa324c4c66bda681cc7510e0ba42202486baa8d0ed4b6182  v2023.07.02.zip' > v2023.zip.sum
 if [[ $(sha512sum -c v2023.zip.sum) == 'v2023.07.02.zip: OK' ]]; then sleep 0; else exit 1; fi;
@@ -19,12 +18,12 @@ wget https://github.com/crust-firmware/crust/archive/refs/tags/v0.6.zip
 echo 'e392498b445cc4a8f376cf14d0fa336fc28349bf2bc48db1c3f5afded56f152b843241eb374d5291595efd9e6ce74cf55fcaab0693be4212b1a8b7870ad92e2d  v0.6.zip' > v0.6.zip.sum
 if [[ $(sha512sum -c v0.6.zip.sum) == 'v0.6.zip: OK' ]]; then sleep 0; else exit 1; fi;
 unzip v202*.zip
-unzip v2.*.zip
 unzip v0.*.zip
-cd arm-trusted-firmware-*
+cd arm-trusted-firmware
+echo "Entering TF-A ------"
 make realclean
 make PLAT=sun50i_a64 DEBUG=1 bl31
-export BL31=/tmp/arm-trusted-firmware-2.9/build/sun50i_a64/debug/bl31/bl31.elf
+export BL31=/tmp/arm-trusted-firmware/build/sun50i_a64/debug/bl31/bl31.elf
 cd ..
 cd crust-0.*
 echo "Entering Crust ------"
@@ -35,20 +34,19 @@ export CROSS_COMPILE=
 cd ..
 cd u-boot-202*
 echo "Entering U-Boot ------"
-echo "CONFIG_SYS_SPL_MALLOC_SIZE=0x2000000" >> configs/pinephone_defconfig
-echo "CONFIG_MFD_AXP20X=y" >> configs/pinephone_defconfig
-echo "Configs for U-Boot ------"
 cat configs/pinephone_defconfig
 make pinephone_defconfig && make -j$(nproc) all
 sha512sum u-boot-sunxi-with-spl.bin
 dd if=u-boot-sunxi-with-spl.bin of=/dev/mmcblk1 bs=8k seek=1
 sha512sum u-boot-sunxi-with-spl.bin > /tmp/u-boot-sunxi-with-spl.bin.sum
 cp u-boot-sunxi-with-spl.bin /tmp/u-boot-sunxi-with-spl.bin
+cd ..
+zip -0 build.zip u-boot-sunxi-with-spl.bin u-boot-sunxi-with-spl.bin.sum
 sync
 popd
-rm -f u-boot-sunxi-with-spl.bin && zip -0 build.zip /tmp/u-boot-sunxi-with-spl.bin /tmp/u-boot-sunxi-with-spl.bin.sum
+cp /tmp/build.zip build.zip
 git status
 git add -A && git status && git commit -a -S -m "Successful Build of U-Boot with TF-A & SCP"
 git push
 apt remove --purge build-essential bc zip unzip bison flex libssl-dev gcc-arm-linux-gnueabi gcc-or1k-elf binutils-or1k-elf device-tree-compiler swig python3-pyelftools python3-dev -y && apt autoremove -y
-rm -f -r /tmp/u-boot-202* && rm -f /tmp/lts-* && rm -f /tmp/v2* && rm -f /tmp/v0* && rm -f -r /tmp/crust-0.* && rm -f -r /tmp/arm-trusted-firmware-* && rm -f -r /tmp/u-boot*
+rm -f -r /tmp/u-boot-202* && rm -f /tmp/lts-* && rm -f /tmp/v0* && rm -f -r /tmp/crust-0.* && rm -f -r /tmp/arm-trusted-firmware* && rm -f -r /tmp/u-boot*
