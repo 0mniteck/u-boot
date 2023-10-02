488649d4836b54f6a1a9611559cca2eed68ee13c
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2023-10-02 12:40:19 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -10,9 +10,9 @@ git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
 rm -f build.zip
 pushd /tmp/
 apt update && apt install build-essential bc zip unzip bison flex libssl-dev device-tree-compiler swig python3-pyelftools python3-dev -y
-wget https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/v2.9.zip
-echo '07e2500d5a64d4ebce5e5d7a934bdb4e911457402a84a8ca0070e42a65fe424596bb0995d03122867e08d459933f45eb7dd5478a5fcccd03afd16625e0dc2d3d  v2.9.zip' > v2.zip.sum
-if [[ $(sha512sum -c v2.zip.sum) == 'v2.9.zip: OK' ]]; then sleep 0; else exit 1; fi;
+wget https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/v2.8.zip
+echo 'e75aa4294fa9047bbc09795f0c1689f324aaddd8cb7880cdc1896f6a36b3a1f55761e5db8cca8e5a78014f269dcb0db4d893039077700f81347e70b843ab8dc4  v2.8.zip' > v2.zip.sum
+if [[ $(sha512sum -c v2.zip.sum) == 'v2.8.zip: OK' ]]; then sleep 0; else exit 1; fi;
 wget https://github.com/u-boot/u-boot/archive/refs/tags/v2023.10.zip
 echo '256e83b931005b3d596ec10c0be74daa3ad433e0e0fc851dae2c209e70d910ad3767c9ce5ba95d1feee362bb4365f056b67ccca1a88fc324471681f99bc4f403  v2023.10.zip' > v2023.zip.sum
 if [[ $(sha512sum -c v2023.zip.sum) == 'v2023.10.zip: OK' ]]; then sleep 0; else exit 1; fi;
@@ -22,7 +22,7 @@ cd arm-trusted-firmware-*
 echo "Entering TF-A ------"
 make realclean
 make PLAT=sun50i_a64 bl31
-export BL31=/tmp/arm-trusted-firmware-2.9/build/sun50i_a64/release/bl31/bl31.elf
+export BL31=/tmp/arm-trusted-firmware-2.8/build/sun50i_a64/release/bl31/bl31.elf
 cd ..
 echo "Bypassing Crust ------"
 export SCP=/dev/null
