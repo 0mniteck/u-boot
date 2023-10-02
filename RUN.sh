22de5804ad4c25e6c11d9bb04fd3ed93d8e6de2a
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2023-10-02 12:25:51 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -10,19 +10,19 @@ git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
 rm -f build.zip
 pushd /tmp/
 apt update && apt install build-essential bc zip unzip bison flex libssl-dev device-tree-compiler swig python3-pyelftools python3-dev -y
-wget https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/lts-v2.8.8.zip
-echo '767cf7af72653432d27043b558fc00550acbfa7e9bbb8c9c7f68c2e15c23ac6af6e28256d7a1912189803371947e824bdb4e5166fb3a7e64097bc838137bc1e0  lts-v2.8.8.zip' > v2.zip.sum
-if [[ $(sha512sum -c v2.zip.sum) == 'lts-v2.8.8.zip: OK' ]]; then sleep 0; else exit 1; fi;
-wget https://github.com/u-boot/u-boot/archive/refs/tags/v2023.07.02.zip
-echo '3293f165ea9b381d4c1e86a40585a9e5b242da2a37f19b592e23983c9a92ba76a3e4c9b8c56dfd4faa324c4c66bda681cc7510e0ba42202486baa8d0ed4b6182  v2023.07.02.zip' > v2023.zip.sum
-if [[ $(sha512sum -c v2023.zip.sum) == 'v2023.07.02.zip: OK' ]]; then sleep 0; else exit 1; fi;
+https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/v2.9.zip
+echo '07e2500d5a64d4ebce5e5d7a934bdb4e911457402a84a8ca0070e42a65fe424596bb0995d03122867e08d459933f45eb7dd5478a5fcccd03afd16625e0dc2d3d  v2.9.zip' > v2.zip.sum
+if [[ $(sha512sum -c v2.zip.sum) == 'v2.9.zip: OK' ]]; then sleep 0; else exit 1; fi;
+wget https://github.com/u-boot/u-boot/archive/refs/tags/v2023.10.zip
+echo '256e83b931005b3d596ec10c0be74daa3ad433e0e0fc851dae2c209e70d910ad3767c9ce5ba95d1feee362bb4365f056b67ccca1a88fc324471681f99bc4f403  v2023.10.zip' > v2023.zip.sum
+if [[ $(sha512sum -c v2023.zip.sum) == 'v2023.10.zip: OK' ]]; then sleep 0; else exit 1; fi;
 unzip v202*.zip
-unzip lts*.zip
+unzip v2.*.zip
 cd arm-trusted-firmware-*
 echo "Entering TF-A ------"
 make realclean
 make PLAT=sun50i_a64 bl31
-export BL31=/tmp/arm-trusted-firmware-lts-v2.8.8/build/sun50i_a64/release/bl31/bl31.elf
+export BL31=/tmp/arm-trusted-firmware-2.9/build/sun50i_a64/release/bl31/bl31.elf
 cd ..
 echo "Bypassing Crust ------"
 export SCP=/dev/null
@@ -42,4 +42,4 @@ git status
 git add -A && git status && git commit -a -S -m "Successful Build of U-Boot with TF-A"
 git push
 apt remove --purge build-essential bc zip unzip bison flex libssl-dev device-tree-compiler swig python3-pyelftools python3-dev -y && apt autoremove -y
-rm -f -r /tmp/u-boot-202* && rm -f /tmp/lts-* && rm -f /tmp/v2* && rm -f -r /tmp/arm-trusted-firmware* && rm -f -r /tmp/u-boot* && rm -f /tmp/build*
+rm -f -r /tmp/u-boot-202* && rm -f /tmp/v2* && rm -f -r /tmp/arm-trusted-firmware* && rm -f -r /tmp/u-boot* && rm -f /tmp/build*
