a80a3d08d4764a6ef41fc998edc369e185426db7
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2023-10-18 12:43:05 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -6,22 +6,26 @@
 ##		  By: Shant Tchatalbachian
 ##
 
+git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
+rm -f spi_combined.zip
 pushd /tmp/
 apt update && apt install build-essential bc zip unzip bison flex libssl-dev gcc-arm-none-eabi device-tree-compiler swig python3-pyelftools python3-dev -y
-wget https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/v2.9.zip
-echo '07e2500d5a64d4ebce5e5d7a934bdb4e911457402a84a8ca0070e42a65fe424596bb0995d03122867e08d459933f45eb7dd5478a5fcccd03afd16625e0dc2d3d  v2.9.zip' > v2.zip.sum
-if [[ $(sha512sum -c v2.zip.sum) == 'v2.9.zip: OK' ]]; then sleep 0; else exit 1; fi;
-wget https://github.com/u-boot/u-boot/archive/refs/tags/v2023.07.02.zip
-echo '3293f165ea9b381d4c1e86a40585a9e5b242da2a37f19b592e23983c9a92ba76a3e4c9b8c56dfd4faa324c4c66bda681cc7510e0ba42202486baa8d0ed4b6182  v2023.07.02.zip' > v2023.zip.sum
-if [[ $(sha512sum -c v2023.zip.sum) == 'v2023.07.02.zip: OK' ]]; then sleep 0; else exit 1; fi;
+wget https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/lts-v2.8.9.zip
+echo '82fdd2ce34bba4691448c446ee8ac76d1afe4816187b443037e537232580156a30ea7d48698ee4d16552824fc65e3ed3a9a33b0827a6491f81937ba31b6a5e6d  lts-v2.8.9.zip' > v2.zip.sum
+if [[ $(sha512sum -c v2.zip.sum) == 'lts-v2.8.9.zip: OK' ]]; then sleep 0; else exit 1; fi;
+wget https://github.com/u-boot/u-boot/archive/refs/tags/v2023.10.zip
+echo '256e83b931005b3d596ec10c0be74daa3ad433e0e0fc851dae2c209e70d910ad3767c9ce5ba95d1feee362bb4365f056b67ccca1a88fc324471681f99bc4f403  v2023.10.zip' > v2023.zip.sum
+if [[ $(sha512sum -c v2023.zip.sum) == 'v2023.10.zip: OK' ]]; then sleep 0; else exit 1; fi;
 unzip v202*.zip
-unzip v2.*.zip
+unzip lts-v2.*.zip
 cd arm-trusted-firmware-*
+echo "Entering TF-A ------"
 make realclean
 make PLAT=rk3399 bl31
 export BL31=/tmp/arm-trusted-firmware-2.9/build/rk3399/release/bl31/bl31.elf
 cd ..
 cd u-boot-202*
+echo "Entering U-Boot ------"
 sed -i 's/CONFIG_BAUDRATE=1500000/CONFIG_BAUDRATE=115200/' configs/rockpro64-rk3399_defconfig
 make rockpro64-rk3399_defconfig && make -j$(nproc) all
 image_name="spi_idbloader.img"
@@ -38,12 +42,14 @@ sha512sum spi_combined.img > /mnt/spi_combined.img.sum
 sha512sum spi_combined.img > /tmp/spi_combined.img.sum
 cp spi_combined.img /mnt/spi_combined.img
 cp spi_combined.img /tmp/spi_combined.img
+cd ..
+zip -0 spi_combined.zip spi_combined.img spi_combined.img.sum
 sync
 umount /mnt
 popd
-rm -f -r spi_combined.zip && zip -0 spi_combined.zip /tmp/spi_combined.img /tmp/spi_combined.img.sum
+cp /tmp/spi_combined.zip spi_combined.zip
 git status
 git add -A && git status && git commit -a -S -m "Successful Build of U-Boot with TF-A"
 git push
 apt remove --purge build-essential bc zip unzip bison flex libssl-dev gcc-arm-none-eabi device-tree-compiler swig python3-pyelftools python3-dev -y && apt autoremove -y
-rm -f -r /tmp/u-boot-202* && rm -f /tmp/lts-* && rm -f /tmp/v2* && rm -f -r /tmp/arm-trusted-firmware-* && rm -f -r /tmp/spi_*
+rm -f -r /tmp/u-boot-202* && rm -f /tmp/lts-* && rm -f /tmp/lts-v2* && rm -f -r /tmp/arm-trusted-firmware-* && rm -f -r /tmp/spi_*
