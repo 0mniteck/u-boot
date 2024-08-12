89255e809ca4f9ce389647107349c19b56a5fc06
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-08-12 14:36:00 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -10,28 +10,28 @@ git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
 rm -f spi_combined.zip
 pushd /tmp/
 apt update && apt install build-essential bc zip unzip bison flex libssl-dev gcc-arm-none-eabi gcc-arm-linux-gnueabihf device-tree-compiler swig python3-pyelftools python3-setuptools python3-dev parted dosfstools libncurses-dev -y
-wget https://github.com/OP-TEE/optee_os/archive/refs/tags/4.1.0.zip
-echo '68fc9a141abb491ca82928cf128f91ace94b74012281618bcb389b0c2947f495c8d2938cad6ab3028981ba36fb4f0be12eb8ec8011d53291a02566236d707a11  4.1.0.zip' > 4.zip.sum
-if [[ $(sha512sum -c 4.zip.sum) == '4.1.0.zip: OK' ]]; then echo 'OP-TEE Checksum Matched!'; else exit 1; fi;
-wget https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/lts-v2.10.2.zip
-echo '56288e2d6f4d105ae18a0a07cbf7d8dae7cb868e486a992bb7f168115b823bf111f51cdfecfc97b769747374676e9de74e66ba4277889d6292a3617a1f82f656 lts-v2.10.2.zip' > v2.zip.sum
-if [[ $(sha512sum -c v2.zip.sum) == 'lts-v2.10.2.zip: OK' ]]; then echo 'ATF Checksum Matched!'; else exit 1; fi;
-wget https://github.com/u-boot/u-boot/archive/refs/tags/v2023.07.02.zip
-echo '3293f165ea9b381d4c1e86a40585a9e5b242da2a37f19b592e23983c9a92ba76a3e4c9b8c56dfd4faa324c4c66bda681cc7510e0ba42202486baa8d0ed4b6182  v2023.07.02.zip' > v2024.zip.sum
-if [[ $(sha512sum -c v2024.zip.sum) == 'v2023.07.02.zip: OK' ]]; then echo 'U-Boot Checksum Matched!'; else exit 1; fi;
+wget https://github.com/OP-TEE/optee_os/archive/refs/tags/4.3.0.zip
+echo '04a2e85947283e49a79cb8d60fde383df28303a9be15080a7f5354268b01f16405178c0c570e253256c3be8e3084d812c8b46b6dc2cb5c8eb3bde8d2ba4c380e  4.3.0.zip' > 4.zip.sum
+if [[ $(sha512sum -c 4.zip.sum) == '4.3.0.zip: OK' ]]; then echo 'OP-TEE Checksum Matched!'; else echo 'OP-TEE Checksum Mismatched!' & exit 1; fi;
+wget https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/lts-v2.10.4.zip
+echo '5252dc59f1133d9c3fae5560954d9810e97a7e3b018522fddea584343d742a110c65678115cb0f554c201b5f7326353eec9a54031485156b6ca0788f53d33882  lts-v2.10.4.zip' > v2.zip.sum
+if [[ $(sha512sum -c v2.zip.sum) == 'lts-v2.10.4.zip: OK' ]]; then echo 'ATF Checksum Matched!'; else echo 'ATF Checksum Mismatched!' & exit 1; fi;
+wget https://github.com/u-boot/u-boot/archive/refs/tags/v2024.07.zip
+echo '0a3e614ba0fd14224f52a8ad3e68e22df08f6e02c43e9183a459d80b4f37b4f384a4bfef7627a3863388fcffb1472c38d178810bed401f63eb8b5d0a21456603  v2024.07.zip' > v2024.zip.sum
+if [[ $(sha512sum -c v2024.zip.sum) == 'v2024.07.zip: OK' ]]; then echo 'U-Boot Checksum Matched!'; else echo 'U-Boot Checksum Mismatched!' & exit 1; fi;
 unzip 4.*.*.zip
 unzip v202*.zip
 unzip lts*.zip
 cd optee_os-*
 echo "Entering OP-TEE ------"
 make -j$(nproc) PLATFORM=rockchip-rk3399 CFG_ARM64_core=y
-export TEE=/tmp/optee_os-4.1.0/out/arm-plat-rockchip/core/tee.bin
+export TEE=/tmp/optee_os-4.3.0/out/arm-plat-rockchip/core/tee.bin
 cd ..
 cd arm-trusted-firmware-*
 echo "Entering TF-A ------"
 make realclean
 make PLAT=rk3399 bl31
-export BL31=/tmp/arm-trusted-firmware-lts-v2.10.2/build/rk3399/release/bl31/bl31.elf
+export BL31=/tmp/arm-trusted-firmware-lts-v2.10.4/build/rk3399/release/bl31/bl31.elf
 cd ..
 cd u-boot-202*
 echo "Entering U-Boot ------"
