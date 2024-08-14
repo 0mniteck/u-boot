ad1e3f9daa9ce60d19d5fe01dbb0e4b858f3f6d3
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-08-13 17:30:16 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -74,7 +74,7 @@ popd
 cp /tmp/spi_combined.zip spi_combined.zip
 git status && git add -A && git status
 read -p "Continue -->"
-git commit -a -S -m "Successful Build of U-Boot $(date -d $(date +%D) +%s) W/ TF-A & OP-TEE For The RockPro64"
+git commit -a -S -m "Successful Build of U-Boot $(date +%D) W/ TF-A & OP-TEE For The RockPro64"
 git push --set-upstream origin RP64-rk3399-A
 cd ..
 apt remove --purge build-essential bc zip unzip bison flex libssl-dev gcc-arm-none-eabi gcc-arm-linux-gnueabihf device-tree-compiler swig python3-pyelftools python3-setuptools python3-dev parted dosfstools libncurses-dev -y && apt autoremove -y
