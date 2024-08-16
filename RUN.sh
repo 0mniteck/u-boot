69a627b655020155ebd3e836f0fcafc4bc5b33da
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-08-16 16:00:06 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -10,23 +10,21 @@ OPT_VER=4.3.0;
 ATF_VER=2.10.4;
 UB_VER=2024.07;
 SOURCE_DATE_EPOCH=$(date -d $(date +%D) +%s);
-BUILD_MESSAGE_TIMESTAMP='"'$(date -d $(date +%D))'"'
+BUILD_MESSAGE_TIMESTAMP='"'$(date -d $(date +%D))'"';
 
 git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
 rm -f spi_combined.zip
+cp 0001-rockchip-rk3399-fix-SPI-NOR-flash-not-found-in-U-Boo.patch /tmp/0001-rockchip-rk3399.patch
 pushd /tmp/
-apt update && apt install acpica-tools adb autoconf automake bc binfmt-support bison build-essential ccache coccinelle cpio cscope curl device-tree-compiler dfu-util dosfstools e2tools efitools expect fakeroot fastboot fdisk flex gcc gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf gcc-arm-none-eabi gdisk git git-lfs libattr1-dev libcap-ng-dev libfdt-dev libftdi-dev libgmp3-dev libgnutls28-dev libhidapi-dev liblz4-tool libmbedtls-dev libmpc-dev libncurses-dev libpixman-1-dev libpython3-dev libslirp-dev libssl-dev libtool libusb-1.0-0-dev lz4 lzma lzma-alone make mtools netcat-openbsd ninja-build openssl parted pkg-config python3 python3-asteval python3-coverage python3-dev python3-filelock python3-pip python3-pkg-resources python3-pycryptodome python3-pytest python3-pyelftools python3-pytest-xdist python3-setuptools python3-subunit python3-testtools python3-virtualenv python-is-python3 rsync swig u-boot-tools udev unzip uuid-dev uuid-runtime wget xz-utils zip zlib1g-dev -y
+apt update && apt install bc bison build-essential device-tree-compiler dosfstools flex gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf gcc-arm-none-eabi libncurses-dev libssl-dev parted python3-dev python3-pyelftools python3-setuptools swig unzip zip -y
 wget https://github.com/OP-TEE/optee_os/archive/refs/tags/$(echo $OPT_VER).zip
 echo '04a2e85947283e49a79cb8d60fde383df28303a9be15080a7f5354268b01f16405178c0c570e253256c3be8e3084d812c8b46b6dc2cb5c8eb3bde8d2ba4c380e  '$(echo $OPT_VER)'.zip' > $(echo $OPT_VER).zip.sum
 if [[ $(sha512sum -c $(echo $OPT_VER).zip.sum) == $(echo $OPT_VER)'.zip: OK' ]]; then echo 'OP-TEE Checksum Matched!'; else echo 'OP-TEE Checksum Mismatched!' & exit 1; fi;
 wget https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/lts-v$(echo $ATF_VER).zip
 echo '5252dc59f1133d9c3fae5560954d9810e97a7e3b018522fddea584343d742a110c65678115cb0f554c201b5f7326353eec9a54031485156b6ca0788f53d33882  lts-v'$(echo $ATF_VER)'.zip' > v$(echo $ATF_VER).zip.sum
 if [[ $(sha512sum -c v$(echo $ATF_VER).zip.sum) == 'lts-v'$(echo $ATF_VER)'.zip: OK' ]]; then echo 'ATF Checksum Matched!'; else echo 'ATF Checksum Mismatched!' & exit 1; fi;
-wget https://github.com/u-boot/u-boot/archive/refs/tags/v$(echo $UB_VER).zip
-echo '0a3e614ba0fd14224f52a8ad3e68e22df08f6e02c43e9183a459d80b4f37b4f384a4bfef7627a3863388fcffb1472c38d178810bed401f63eb8b5d0a21456603  v'$(echo $UB_VER)'.zip' > v$(echo $UB_VER).zip.sum
-if [[ $(sha512sum -c v$(echo $UB_VER).zip.sum) == 'v'$(echo $UB_VER)'.zip: OK' ]]; then echo 'U-Boot Checksum Matched!'; else echo 'U-Boot Checksum Mismatched!' & exit 1; fi;
+git clone https://github.com/u-boot/u-boot.git -b v$(echo $UB_VER)
 unzip $(echo $OPT_VER).zip
-unzip v$(echo $UB_VER).zip
 unzip lts-v$(echo $ATF_VER).zip
 cd optee_os-$(echo $OPT_VER)
 echo "Entering OP-TEE ------"
@@ -36,14 +34,15 @@ cd ..
 cd arm-trusted-firmware-lts-v$(echo $ATF_VER)
 echo "Entering TF-A ------"
 make realclean
-make PLAT=rk3399 bl31
+make BUILD_MESSAGE_TIMESTAMP=$(echo $BUILD_MESSAGE_TIMESTAMP) PLAT=rk3399 bl31
 export BL31=/tmp/arm-trusted-firmware-lts-v$(echo $ATF_VER)/build/rk3399/release/bl31/bl31.elf
 cd ..
-cd u-boot-$(echo $UB_VER)
+cd u-boot
 echo "Entering U-Boot ------"
+git am < ../0001-rockchip-rk3399.patch
 sed -i 's/CONFIG_BAUDRATE=1500000/CONFIG_BAUDRATE=115200/' configs/rockpro64-rk3399_defconfig
 make rockpro64-rk3399_defconfig
-make -j$(nproc) all
+make SOURCE_DATE_EPOCH=$(echo $SOURCE_DATE_EPOCH) -j$(nproc) all
 image_name="spi_idbloader.img"
 combined_name="spi_combined.img"
 tools/mkimage -n rk3399 -T rkspi -d tpl/u-boot-tpl.bin:spl/u-boot-spl.bin "${image_name}"
@@ -53,6 +52,7 @@ dd if=/dev/zero of="${image_name}" conv=notrunc bs=1 count=1 seek=${padsize}
 cat ${image_name} u-boot.itb > "${combined_name}"
 sha512sum spi_combined.img
 sha512sum u-boot-rockchip.bin
+sha512sum u-boot-rockchip-spi.bin
 read -p "Insert any SD Card, Then Press Enter to Continue"
 dd if=/dev/zero of=/dev/mmcblk1 bs=1M count=2000 status=progress
 parted /dev/mmcblk1 mktable gpt mkpart P1 fat32 16MB 1G -s && sleep 3
@@ -67,18 +67,27 @@ sha512sum u-boot-rockchip.bin
 sha512sum u-boot-rockchip.bin > /mnt/u-boot-rockchip.bin.sum
 sha512sum u-boot-rockchip.bin > /tmp/u-boot-rockchip.bin.sum
 cp u-boot-rockchip.bin /tmp/u-boot-rockchip.bin
+sha512sum u-boot-rockchip-spi.bin
+sha512sum u-boot-rockchip-spi.bin > /mnt/u-boot-rockchip-spi.bin.sum
+sha512sum u-boot-rockchip-spi.bin > /tmp/u-boot-rockchip-spi.bin.sum
+cp u-boot-rockchip-spi.bin /mnt/u-boot-rockchip-spi.bin
+cp u-boot-rockchip-spi.bin /tmp/u-boot-rockchip-spi.bin
 sync
 umount /mnt
 dd if=u-boot-rockchip.bin of=/dev/mmcblk1 seek=64 conv=notrunc status=progress
 cd ..
-zip -0 spi_combined.zip spi_combined.img spi_combined.img.sum u-boot-rockchip.bin u-boot-rockchip.bin.sum
+zip -0 spi_combined.zip spi_combined.img spi_combined.img.sum u-boot-rockchip.bin u-boot-rockchip.bin.sum u-boot-rockchip-spi.bin u-boot-rockchip-spi.bin.sum
+sha512sum spi_combined.zip
 sync
 popd
+sha512sum /tmp/spi_combined.zip > /tmp/spi_combined.zip.sum
+cp /tmp/spi_combined.zip.sum spi_combined.zip.sum
 cp /tmp/spi_combined.zip spi_combined.zip
+sha512sum spi_combined.zip
 git status && git add -A && git status
 read -p "Continue -->"
-git commit -a -S -m "Successful Build of U-Boot v$(echo $UB_VER) at $BUILDTIME W/ TF-A $(echo $ATF_VER) & OP-TEE $(echo $OPT_VER) For The RockPro64"
+git commit -a -S -m "Successful Build of U-Boot v$(echo $UB_VER) at "$(echo $BUILD_MESSAGE_TIMESTAMP)" W/ TF-A $(echo $ATF_VER) & OP-TEE $(echo $OPT_VER) For The RockPro64"
 git push --set-upstream origin RP64-rk3399-A
 cd ..
-apt remove --purge acpica-tools adb autoconf automake bc binfmt-support bison build-essential ccache coccinelle cscope device-tree-compiler dfu-util dosfstools e2tools efitools expect fakeroot fastboot flex gcc gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf gcc-arm-none-eabi libattr1-dev libcap-ng-dev libfdt-dev libftdi-dev libgmp3-dev libgnutls28-dev libhidapi-dev liblz4-tool libmbedtls-dev libmpc-dev libncurses-dev libpixman-1-dev libpython3-dev libslirp-dev libssl-dev libtool libusb-1.0-0-dev lz4 lzma lzma-alone make mtools netcat-openbsd ninja-build parted pkg-config python3-asteval python3-coverage python3-dev python3-filelock python3-pip python3-pycryptodome python3-pytest python3-pyelftools python3-pytest-xdist python3-setuptools python3-subunit python3-testtools python3-virtualenv python-is-python3 rsync swig u-boot-tools uuid-dev uuid-runtime zlib1g-dev -y && apt autoremove -y
-rm -f -r /tmp/u-boot* && rm -f /tmp/4.* && rm -f /tmp/lts* && rm -f /tmp/v2* && rm -f -r /tmp/arm-trusted-firmware-* && rm -f -r /tmp/optee_os-* && rm -f /tmp/spi_* && rm -f /tmp/rk* && rm -f -r U-Boot
+apt remove --purge bc bison build-essential device-tree-compiler dosfstools flex gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf gcc-arm-none-eabi libncurses-dev libssl-dev parted python3-dev python3-pyelftools python3-setuptools swig unzip zip -y && apt autoremove -y
+rm -f -r /tmp/u-boot* && rm -f /tmp/4.* && rm -f /tmp/lts* && rm -f /tmp/v2* && rm -f -r /tmp/arm-trusted-firmware-* && rm -f -r /tmp/optee_os-* && rm -f /tmp/spi_* && rm -f /tmp/rk* && rm -f /tmp/000* && rm -f -r U-Boot
