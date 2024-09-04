a47fddd846aa0c7923ad80a5973b581e8b7af1d3
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-09-03 19:38:29 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -73,10 +73,11 @@ git apply ../0001-rockchip-rk3399.patch && echo "Patched SPI bug"
 rm tools/logos/denx.bmp && rm drivers/video/u_boot_logo.bmp
 cp /tmp/logo.bmp tools/logos/denx.bmp && cp /tmp/logo.bmp drivers/video/u_boot_logo.bmp
 sed -i 's/CONFIG_BAUDRATE=1500000/CONFIG_BAUDRATE=115200/' configs/rockpro64-rk3399_defconfig
-# echo "CONFIG_ARMV8_SEC_FIRMWARE_SUPPORT" >> configs/rockpro64-rk3399_defconfig
-# echo "CONFIG_FIT_SIGNATURE" >> configs/rockpro64-rk3399_defconfig
-# echo "CONFIG_RSA" >> configs/rockpro64-rk3399_defconfig
-# echo "CONFIG_ECDSA" >> configs/rockpro64-rk3399_defconfig
+# echo "CONFIG_ARMV8_SEC_FIRMWARE_SUPPORT=y" >> configs/rockpro64-rk3399_defconfig
+# echo "CONFIG_ARM_FFA_TRANSPORT=y" >> configs/rockpro64-rk3399_defconfig
+# echo "CONFIG_FIT_SIGNATURE=y" >> configs/rockpro64-rk3399_defconfig
+# echo "CONFIG_RSA=y" >> configs/rockpro64-rk3399_defconfig
+# echo "CONFIG_ECDSA=y" >> configs/rockpro64-rk3399_defconfig
 # echo "CONFIG_BOOTM_EFI=y" >> configs/rockpro64-rk3399_defconfig
 echo "CONFIG_STACKPROTECTOR=y" >> configs/rockpro64-rk3399_defconfig
 echo "TPL_STACKPROTECTOR=y" >> configs/rockpro64-rk3399_defconfig
