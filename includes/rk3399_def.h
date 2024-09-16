759206f9b81effd93bb4f656ebf09c4a145e5b69
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-09-16 16:31:07 -0700

    Update rk3399_def.h

--- includes/rk3399_def.h
+++ includes/rk3399_def.h
@@ -9,6 +9,10 @@
 #define RK3399_DEF_H
 
 #include <addressmap.h>
+#if SPM_MM
+#include <plat/arm/common/arm_spm_def.h>
+#include <services/spm_mm_partition.h>
+#endif
 
 #define RK3399_PRIMARY_CPU		0x0
 
