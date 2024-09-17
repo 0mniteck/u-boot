bb3944d5024d97340548b3ebe7b19bf184368dfe
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-09-16 18:04:54 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -87,7 +87,7 @@ cp /tmp/platform.mk plat/rockchip/rk3399/platform.mk
 cp /tmp/rk3399_def.h plat/rockchip/rk3399/rk3399_def.h
 cp /tmp/platform_def.h plat/rockchip/rk3399/include/platform_def.h
 # cp /tmp/spm_mm_setup.c services/std_svc/spm/spm_mm/spm_mm_setup.c
-BL33=tee.bin BL32=BL32_AP_MM.fd make BUILD_MESSAGE_TIMESTAMP="$(echo '"'$BUILD_MESSAGE_TIMESTAMP'"')" SPM_MM=1 EL3_EXCEPTION_HANDLING=1 ENABLE_SVE_FOR_NS=0 CTX_INCLUDE_FPREGS=1 ARM_BL31_IN_DRAM=1 PLAT=rk3399 bl31
+BL33=tee.bin BL32=BL32_AP_MM.fd make BUILD_MESSAGE_TIMESTAMP="$(echo '"'$BUILD_MESSAGE_TIMESTAMP'"')" SPM_MM=1 EL3_EXCEPTION_HANDLING=1 ENABLE_SVE_FOR_NS=0 ARM_BL31_IN_DRAM=1 PLAT=rk3399 bl31
 # make BUILD_MESSAGE_TIMESTAMP="$(echo '"'$BUILD_MESSAGE_TIMESTAMP'"')" PLAT=rk3399 bl31
 export BL31=/tmp/arm-trusted-firmware-lts-v$(echo $ATF_VER)/build/rk3399/release/bl31/bl31.elf
 cd ..
