cd55fd5621f91fc568fdc46100369aec23583bb3
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-09-17 12:45:38 -0700

    Update spm_mm_setup.c

--- includes/spm_mm_setup.c
+++ includes/spm_mm_setup.c
@@ -17,7 +17,6 @@
 #include <platform_def.h>
 #include <plat/common/common_def.h>
 #include <plat/common/platform.h>
-#include <plat/arm/common/arm_spm_def.h>
 #include <services/spm_mm_partition.h>
 
 #include "spm_common.h"
