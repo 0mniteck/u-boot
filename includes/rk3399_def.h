8ccadc223c1d6a51460e8508fb8c99671e080a4d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-09-18 17:18:42 -0700

    Update rk3399_def.h

--- includes/rk3399_def.h
+++ includes/rk3399_def.h
@@ -67,7 +67,7 @@
 #define PLAT_PRI_BITS			3
 #define PLAT_SP_IMAGE_MMAP_REGIONS	7
 #define PLAT_SP_IMAGE_MAX_XLAT_TABLES	10
-#define PLAT_SP_PRI			0x10
+#define PLAT_SP_PRI			0x20
 #  endif
 # endif
 
