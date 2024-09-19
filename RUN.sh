e35d39375ed14b09ca8c430c6f5dae273b92d212
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-09-19 15:08:12 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -65,6 +65,7 @@ if [ -f Builds/tee.bin ]; then
   export TEE=/tmp/tee.bin
   pushd /tmp/
 else
+  pushd /tmp/
   wget https://github.com/OP-TEE/optee_os/archive/refs/tags/$(echo $OPT_VER).zip
   echo '04a2e85947283e49a79cb8d60fde383df28303a9be15080a7f5354268b01f16405178c0c570e253256c3be8e3084d812c8b46b6dc2cb5c8eb3bde8d2ba4c380e  '$(echo $OPT_VER)'.zip' > $(echo $OPT_VER).zip.sum
   if [[ $(sha512sum -c $(echo $OPT_VER).zip.sum) == $(echo $OPT_VER)'.zip: OK' ]]; then echo 'OP-TEE Checksum Matched!'; else echo 'OP-TEE Checksum Mismatched!' & exit 1; fi;
@@ -76,7 +77,6 @@ else
   export TEE=/tmp/optee_os-$(echo $OPT_VER)/out/arm-plat-rockchip/core/tee.bin
   cd ..
   ln -s /tmp/optee_os-$(echo $OPT_VER)/out/arm-plat-rockchip/core/tee.bin
-  pushd /tmp/
 fi
 wget https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/lts-v$(echo $ATF_VER).zip
 echo '5252dc59f1133d9c3fae5560954d9810e97a7e3b018522fddea584343d742a110c65678115cb0f554c201b5f7326353eec9a54031485156b6ca0788f53d33882  lts-v'$(echo $ATF_VER)'.zip' > v$(echo $ATF_VER).zip.sum
