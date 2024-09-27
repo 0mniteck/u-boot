6be5fecf1f514d4a0de895c06b93f2f43656ff80
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-09-27 04:00:09 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -142,22 +142,26 @@ if [ -f Builds/tee.bin ]; then
   pushd /tmp/
 else
   pushd /tmp/
-  wget https://github.com/OP-TEE/optee_os/archive/refs/tags/$(echo $OPT_VER).zip
-  echo '04a2e85947283e49a79cb8d60fde383df28303a9be15080a7f5354268b01f16405178c0c570e253256c3be8e3084d812c8b46b6dc2cb5c8eb3bde8d2ba4c380e  '$(echo $OPT_VER)'.zip' > $(echo $OPT_VER).zip.sum
-  if [[ $(sha512sum -c $(echo $OPT_VER).zip.sum) == $(echo $OPT_VER)'.zip: OK' ]]; then echo 'OP-TEE Checksum Matched!'; else echo 'OP-TEE Checksum Mismatched!' & exit 1; fi;
-  unzip $(echo $OPT_VER).zip
-  cd optee_os-$(echo $OPT_VER)
+  snap install lxd && lxd init --auto && lxc launch ubuntu:24.04 tee
+  sleep 30
+  ufw reload
+  sleep 10
+  lxc exec tee apt update && lxc exec tee -- apt upgrade -y
+  lxc exec tee -- apt install -y adb acpica-tools autoconf automake bc bison build-essential ccache cpio cscope curl device-tree-compiler e2tools expect fastboot flex ftp-upload gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf gcc-arm-none-eabi gdisk git libattr1-dev libcap-ng-dev libfdt-dev libftdi-dev libglib2.0-dev libgmp3-dev libhidapi-dev libmpc-dev libncurses5-dev libpixman-1-dev libslirp-dev libssl-dev libtool libusb-1.0-0-dev make mtools netcat ninja-build python3-cryptography python3-pip python3-pyelftools python3-serial python-is-python3 rsync swig unzip uuid-dev wget xalan xdg-utils xterm xz-utils zlib1g-dev
+  lxc exec tee -- wget https://github.com/OP-TEE/optee_os/archive/refs/tags/$(echo $OPT_VER).zip
+  lxc exec tee -- bash -c "echo '04a2e85947283e49a79cb8d60fde383df28303a9be15080a7f5354268b01f16405178c0c570e253256c3be8e3084d812c8b46b6dc2cb5c8eb3bde8d2ba4c380e  '$(echo $OPT_VER)'.zip' > $(echo $OPT_VER).zip.sum"
+  if [[ $(lxc exec tee -- bash -c "sha512sum -c $(echo $OPT_VER).zip.sum") == $(echo $OPT_VER)'.zip: OK' ]]; then echo 'OP-TEE Checksum Matched!'; else echo 'OP-TEE Checksum Mismatched!' & exit 1; fi;
+  lxc exec tee -- unzip $(echo $OPT_VER).zip
   echo "Entering OP-TEE ------"
-  ln -s /tmp/BL32_AP_MM.fd
-  make -j$(nproc) PLATFORM=rockchip-rk3399 CFG_ARM64_core=y CFG_STMM_PATH=BL32_AP_MM.fd CFG_RPMB_FS=y CFG_RPMB_FS_DEV_ID=1 CFG_CORE_HEAP_SIZE=524288 CFG_TZSRAM_SIZE=0x60000 CFG_RPMB_WRITE_KEY=y \
+  lxc exec tee --cwd /root/optee_os-$(echo $OPT_VER) -- bash -i -c "make -j\$(nproc) PLATFORM=rockchip-rk3399 CFG_ARM64_core=y CFG_STMM_PATH=BL32_AP_MM.fd CFG_RPMB_FS=y CFG_RPMB_FS_DEV_ID=1 CFG_CORE_HEAP_SIZE=524288 CFG_TZSRAM_SIZE=0x60000 CFG_RPMB_WRITE_KEY=y \
   CFG_CORE_DYN_SHM=y CFG_RPMB_TESTKEY=y CFG_REE_FS_ALLOW_RESET=y CFG_CORE_ARM64_PA_BITS=48 CFG_ENABLE_EMBEDDED_TESTS=y CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_core=aarch64-linux-gnu- \
-  CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- CFG_USER_TA_TARGETS=ta_arm64 CFG_TEE_CORE_LOG_LEVEL=3 CFG_TEE_TA_LOG_LEVEL=3 CFG_SCTLR_ALIGNMENT_CHECK=n DEBUG=1 CFG_EARLY_CONSOLE_BAUDRATE=115200
-  export TEE=/tmp/optee_os-$(echo $OPT_VER)/out/arm-plat-rockchip/core/tee.bin
-  cd ..
-  #ln -s /tmp/optee_os-$(echo $OPT_VER)/out/arm-plat-rockchip/core/tee-header_v2.bin
-  #ln -s /tmp/optee_os-$(echo $OPT_VER)/out/arm-plat-rockchip/core/tee-pager_v2.bin
-  #ln -s /tmp/optee_os-$(echo $OPT_VER)/out/arm-plat-rockchip/core/tee-pageable_v2.bin
-  ln -s /tmp/optee_os-$(echo $OPT_VER)/out/arm-plat-rockchip/core/tee.bin
+  CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- CFG_USER_TA_TARGETS=ta_arm64 CFG_TEE_CORE_LOG_LEVEL=3 CFG_TEE_TA_LOG_LEVEL=3 CFG_SCTLR_ALIGNMENT_CHECK=n DEBUG=1 CFG_EARLY_CONSOLE_BAUDRATE=115200"
+  lxc file pull tee/root/optee_os-$(echo $OPT_VER)/out/arm-plat-rockchip/core/tee.bin /tmp/
+  # lxc file pull tee/root/optee_os-$(echo $OPT_VER)/out/arm-plat-rockchip/core/tee-header_v2.bin /tmp/
+  # lxc file pull tee/root/optee_os-$(echo $OPT_VER)/out/arm-plat-rockchip/core/tee-pager_v2.bin /tmp/
+  # lxc file pull tee/root/optee_os-$(echo $OPT_VER)/out/arm-plat-rockchip/core/tee-pageable_v2.bin /tmp/
+  snap remove lxd --purge
+  export TEE=/tmp/tee.bin
 fi
 wget https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/lts-v$(echo $ATF_VER).zip
 echo '5252dc59f1133d9c3fae5560954d9810e97a7e3b018522fddea584343d742a110c65678115cb0f554c201b5f7326353eec9a54031485156b6ca0788f53d33882  lts-v'$(echo $ATF_VER)'.zip' > v$(echo $ATF_VER).zip.sum
