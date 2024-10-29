5227ef1cc62d71430bdc86ff29d9ade3c819c0f4
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-10-29 10:12:43 -0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -114,6 +114,8 @@ echo "CONFIG_CMD_HASH=y" >> rk3399_defconfig
 ## echo "CONFIG_CMD_EFIDEBUG=y" >> rk3399_defconfig
 echo 'CONFIG_SYS_PROMPT="0MNITECK:~$ "' >> rk3399_defconfig
 echo 'CONFIG_LOCALVERSION=" 0MNITECK"' >> rk3399_defconfig
+#echo 'CONFIG_DEVICE_TREE_INCLUDES=""' >> rk3399_defconfig
+echo 'CONFIG_OF_OVERLAY_LIST="rockchip/rk3399-rockpro64-tpm.dtso"' >> rk3399_defconfig
 popd
 
 if [ -f Builds/sbsign ]; then
@@ -203,8 +205,9 @@ lxc exec ub --cwd /root/u-boot-$(echo $UB_VER) -- make clean
 lxc file push /tmp/bl31.elf ub/root/u-boot-$(echo $UB_VER)/bl31.elf && lxc config set ub environment.BL31=/root/u-boot-$(echo $UB_VER)/bl31.elf && echo "Imported bl31.elf"
 lxc file push /tmp/tee.bin ub/root/u-boot-$(echo $UB_VER)/tee.bin && lxc config set ub environment.TEE=/root/u-boot-$(echo $UB_VER)/tee.bin && echo "Imported tee.bin"
 # lxc file push includes/rk3399-u-boot.dtsi ub/root/u-boot-$(echo $UB_VER)/arch/arm/dts/rk3399-u-boot.dtsi
-lxc file push includes/rk3399-rockpro64.dtsi ub/root/u-boot-$(echo $UB_VER)/dts/upstream/src/arm64/rockchip/rk3399-rockpro64.dtsi && echo "Patched Device Tree for TPM"
 # lxc file push includes/rk3399-rockpro64-u-boot.dtsi ub/root/u-boot-$(echo $UB_VER)/arch/arm/dts/rk3399-rockpro64-u-boot.dtsi
+# lxc file push includes/rk3399-rockpro64.dtsi ub/root/u-boot-$(echo $UB_VER)/dts/upstream/src/arm64/rockchip/rk3399-rockpro64.dtsi && echo "Patched Device Tree for TPM"
+lxc file push includes/rk3399-rockpro64-tpm.dtso ub/root/u-boot-$(echo $UB_VER)/dts/upstream/src/arm64/rockchip/rk3399-rockpro64-tpm.dtso && echo "Overlayed Device Tree for TPM"
 lxc file push includes/efi.var ub/root/u-boot-$(echo $UB_VER)/efi.var && echo "Deployed efi.var"
 lxc exec ub --cwd /root/u-boot-$(echo $UB_VER) -- rm -f tools/logos/denx.bmp
 lxc exec ub --cwd /root/u-boot-$(echo $UB_VER) -- rm -f drivers/video/u_boot_logo.bmp
