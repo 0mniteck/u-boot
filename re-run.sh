ab04b9e4a8ffb3c912a243cbd338562e775144db
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-02 04:21:56 -0700

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -118,12 +118,13 @@ cp u-boot-rockchip-spi.bin /mnt/u-boot-rockchip-spi.bin
 sync && umount /mnt
 dd if=u-boot-rockchip.bin of=/dev/mmcblk1 seek=64 conv=notrunc status=progress
 sync && dd if=/dev/mmcblk1 of=sdcard.img bs=1M count=30 status=progress
-sha512sum sdcard.img > sdcard.img.sum
 popd
+sha512sum Builds/$loc/sdcard.img > Builds/release.sha512sum
 done
 dd if=/dev/zero of=/dev/mmcblk1 bs=1M count=100 status=progress
 dd if=u-boot-rockchip.bin of=/dev/mmcblk1 seek=64 conv=notrunc status=progress
 
+echo "" >> Builds/release.sha512sum
 echo "# 0mniteck's Current GPG Key ID: 287EE837E6ED2DD3" >> Builds/release.sha512sum
 echo "Source Date Epoch: $SOURCE_DATE_EPOCH" >> Builds/release.sha512sum
 echo "Build Complete: "$(date -u '+on %D at %R UTC') && echo "# Build Complete: "$(date -u '+on %D at %R UTC') >> Builds/release.sha512sum
