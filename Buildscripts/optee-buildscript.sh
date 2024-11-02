09dfadd10b847e281a066cf8f132c78b367c61a8
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-01 22:39:18 -0700

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
-
 trap '[[ $pid ]] && kill $pid; exit' EXIT
-
 echo "SOURCE_DATE_EPOCH: ${SOURCE_DATE_EPOCH}"
-
-echo "Entering /TEMPLATE"
-pushd /TEMPLATE
-sha512sum release/*.file && sha512sum release/*.file > release/release.sha512sum
-echo "# 0mniteck's Current GPG Key ID: 287EE837E6ED2DD3" >> release/release.sha512sum
-echo "Source Date Epoch: ${SOURCE_DATE_EPOCH}" >> release/release.sha512sum
-echo "Build Complete: "$(date -u '+on %D at %R UTC') && echo "# Build Complete: "$(date -u '+on %D at %R UTC') >> release/release.sha512sum
-echo "# Container Build System: $(uname -o) $(uname -r) $(uname -m) $(lsb_release -ds) $(uname -v)"  >> release/release.sha512sum
-ls -la release/
+echo "Entering /optee_os-${$OPT_VER}"
+pushd /root/optee_os-${$OPT_VER}
+make -j $(nproc) PLATFORM=rockchip-rk3399 CFG_ARM64_core=y CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_core=aarch64-linux-gnu- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu-
+ls -la /optee_os-${$OPT_VER}/out/arm-plat-rockchip/core/
 popd
