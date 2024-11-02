1a9a815486c1e994e2088602d0e0084bd59f509e
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-02 04:43:30 -0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -8,5 +8,5 @@ echo "Starting Build "$(date -u '+on %D at %R UTC')
 sudo screen -L -Logfile /tmp/builder.log bash -c './re-run.sh '$(($2))
 cp /tmp/builder.log Builds/builder.log && rm -f /tmp/builder.log
 ./git.sh "Build Artifact Added"
-ls -la builds/*
+ls -la Builds/*
 cd ..
