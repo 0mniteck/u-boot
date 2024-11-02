bdfef5766131813051b93b6223f08dbfe0d46114
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-01 21:10:19 -0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git status && git add -A && git status
-git commit -a -S -m "Successful Build of Reproducible Release x.xx.x" && git push --set-upstream origin master
+git commit -a -S -m "Successful Build of Reproducible Release for $1" && git push --set-upstream origin rk3399-Docker
