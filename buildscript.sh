891b5596d554a07f1ce2b9679063489cf2a1a86c
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-02 02:51:14 -0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -3,7 +3,7 @@ sudo apt install bc git git-lfs screen snapd -y
 git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
 git-lfs install
 echo "Starting Build "$(date -u '+on %D at %R UTC')
-./increment.sh $1
+./clean.sh $1
 sudo screen -L -Logfile /tmp/builder.log bash -c './re-run.sh '$(($2))
 cp /tmp/builder.log builds/builder.log && rm -f /tmp/builder.log
 echo "# Base Build System: $(uname -o) $(uname -r) $(uname -p) $(lsb_release -ds) $(lsb_release -cs) $(uname -v)"  >> builds/release.sha512sum
