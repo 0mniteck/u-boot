702b608c866510d2c928a8f4ef0874ab0aeabad5
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-02 03:31:02 -0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -5,7 +5,7 @@ git-lfs install
 echo "Starting Build "$(date -u '+on %D at %R UTC')
 ./clean.sh $1
 sudo screen -L -Logfile /tmp/builder.log bash -c './re-run.sh '$(($2))
-cp /tmp/builder.log builds/builder.log && rm -f /tmp/builder.log
+cp /tmp/builder.log Builds/builder.log && rm -f /tmp/builder.log
 ./git.sh "Build Artifacts Added"
 ls -la builds/*
 cd ..
