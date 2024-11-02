fb3be0b653feb39b6e6451aad8cd62982d6e31cb
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-01 23:08:43 -0700

    Update arm-trusted-buildscript.sh

--- Buildscripts/arm-trusted-buildscript.sh
+++ Buildscripts/arm-trusted-buildscript.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
-
 trap '[[ $pid ]] && kill $pid; exit' EXIT
-
 echo "SOURCE_DATE_EPOCH: ${SOURCE_DATE_EPOCH}"
-
-echo "Entering /TEMPLATE"
-pushd /TEMPLATE
-sha512sum release/*.file && sha512sum release/*.file > release/release.sha512sum
-echo "# 0mniteck's Current GPG Key ID: 287EE837E6ED2DD3" >> release/release.sha512sum
-echo "Source Date Epoch: ${SOURCE_DATE_EPOCH}" >> release/release.sha512sum
-echo "Build Complete: "$(date -u '+on %D at %R UTC') && echo "# Build Complete: "$(date -u '+on %D at %R UTC') >> release/release.sha512sum
-echo "# Container Build System: $(uname -o) $(uname -r) $(uname -m) $(lsb_release -ds) $(uname -v)"  >> release/release.sha512sum
-ls -la release/
+echo "Entering /arm-trusted-firmware-lts-v${ATF_VER}"
+pushd /arm-trusted-firmware-lts-v${ATF_VER}
+make -j$(nproc) BUILD_MESSAGE_TIMESTAMP="$(echo '"'$BUILD_MESSAGE_TIMESTAMP'"')" PLAT=rk3399 bl31
+ls -la /arm-trusted-firmware-lts-v$(echo $ATF_VER)/build/rk3399/release/bl31/
 popd
