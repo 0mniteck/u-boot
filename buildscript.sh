7bc22968c03affa2a84abf03a22961e20c156742
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-02 03:30:10 -0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -6,7 +6,6 @@ echo "Starting Build "$(date -u '+on %D at %R UTC')
 ./clean.sh $1
 sudo screen -L -Logfile /tmp/builder.log bash -c './re-run.sh '$(($2))
 cp /tmp/builder.log builds/builder.log && rm -f /tmp/builder.log
-echo "# Base Build System: $(uname -o) $(uname -r) $(uname -p) $(lsb_release -ds) $(lsb_release -cs) $(uname -v)"  >> builds/release.sha512sum
-awk '{a[i++]=$0}END{for(j=0;j<i-2;j++)print a[j];print a[i-1];print a[i-2]}' builds/release.sha512sum > tmp && mv tmp builds/release.sha512sum
+./git.sh "Build Artifacts Added"
 ls -la builds/*
 cd ..
