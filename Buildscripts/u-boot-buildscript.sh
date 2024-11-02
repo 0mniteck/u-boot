17cedc9c522002dca2a8ea33f697d89da757fc6f
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-02 02:15:05 -0700

    Update u-boot-buildscript.sh

--- Buildscripts/u-boot-buildscript.sh
+++ Buildscripts/u-boot-buildscript.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 trap '[[ $pid ]] && kill $pid; exit' EXIT
 echo "SOURCE_DATE_EPOCH: ${SOURCE_DATE_EPOCH}"
-echo "Entering /TEMPLATE"
-pushd /TEMPLATE
-ls -la release/
+./config.sh
+echo "Entering /RP64/u-boot-$(echo $UB_VER)"
+pushd /RP64/u-boot-$(echo $UB_VER)
+make clean
+cp /efi.var efi.var && echo "Deployed efi.var"
+cp /logo.bmp tools/logos/denx.bmp && cp /logo.bmp drivers/video/u_boot_logo.bmp
+sed -i 's/CONFIG_BAUDRATE=1500000/CONFIG_BAUDRATE=115200/' configs/rockpro64-rk3399_defconfig
+cat /rk3399_defconfig >> configs/rockpro64-rk3399_defconfig
+make rockpro64-rk3399_defconfig
+FORCE_SOURCE_DATE=1 SOURCE_DATE=$SOURCE_DATE SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH make -j $(nproc) all
+ls -la
+popd
+echo "Entering /PBP/u-boot-$(echo $UB_VER)"
+pushd /PBP/u-boot-$(echo $UB_VER)
+make clean
+cp /rk3399-pinebook-pro-u-boot.dtsi arch/arm/dts/rk3399-pinebook-pro-u-boot.dtsi && echo "Patched Device Tree bug"
+cp /efi.var efi.var && echo "Deployed efi.var"
+cp /logo.bmp tools/logos/denx.bmp && cp /logo.bmp drivers/video/u_boot_logo.bmp
+sed -i 's/CONFIG_BAUDRATE=1500000/CONFIG_BAUDRATE=115200/' configs/pinebook-pro-rk3399_defconfig
+cat /rk3399_defconfig >> configs/pinebook-pro-rk3399_defconfig
+make pinebook-pro-rk3399_defconfig
+FORCE_SOURCE_DATE=1 SOURCE_DATE=$SOURCE_DATE SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH make -j $(nproc) all
+ls -la
 popd
