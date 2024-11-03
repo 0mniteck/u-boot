6e1bdfbb4c1b49211c491609fcfe9258cafa70bc
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-02 23:44:11 -0700

    Update u-boot-buildscript.sh

--- Buildscripts/u-boot-buildscript.sh
+++ Buildscripts/u-boot-buildscript.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 trap '[[ $pid ]] && kill $pid; exit' EXIT
 echo "SOURCE_DATE_EPOCH: ${SOURCE_DATE_EPOCH}"
+echo "SOURCE_DATE: ${SOURCE_DATE}"
 $1
 echo "Entering /RP64/u-boot-$(echo $UB_VER)"
 pushd /RP64/u-boot-$(echo $UB_VER)
