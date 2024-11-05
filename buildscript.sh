9cca0554800d23cac807d2a49c149795f1a8b290
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-04 17:52:40 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-chmod +x *.sh
-chmod +x Buildscripts/*.sh
-chmod +x Configs/*.sh
+# chmod +x *.sh
+# chmod +x Buildscripts/*.sh
+# chmod +x Configs/*.sh
+for loc in RP64-rk3399 PBP-rk3399 RP64-rk3399-SB PBP-rk3399-SB
+do
+mkdir Builds/$loc
+done
 sudo apt install bc dosfstools git git-lfs parted screen snapd -y
 snap install syft --classic
 git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
