0c8fc06d7390c05ed6db74802da745e33c8da64d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-05 05:58:42 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -5,6 +5,7 @@ git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
 ./clean.sh $1
 sudo screen -L -Logfile /tmp/builder.log bash -c './re-run.sh '$(($2))
 cp /tmp/builder.log Builds/builder.log && rm -f /tmp/builder.log
+./clean.sh cleanup
 ./git.sh "Build Artifact Added"
 ls -la Builds/*
 cd ..
