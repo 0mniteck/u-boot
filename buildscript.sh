a04a0833129125c9fa11da99d7ad1e9065fe703a
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-05 05:55:29 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sudo apt install bc dosfstools git git-lfs parted screen snapd -y
+sudo apt install bc dosfstools git parted screen snapd -y
 sudo snap install syft --classic
 git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
-git-lfs install
 ./clean.sh $1
 sudo screen -L -Logfile /tmp/builder.log bash -c './re-run.sh '$(($2))
 cp /tmp/builder.log Builds/builder.log && rm -f /tmp/builder.log
