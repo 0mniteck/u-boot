26aee16e82d5bc4a4f520f4f971d0096b17cf3b7
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-05 00:36:01 -0800

    Update u-boot-buildscript.sh

--- Buildscripts/u-boot-buildscript.sh
+++ Buildscripts/u-boot-buildscript.sh
@@ -10,6 +10,7 @@ cp /efi.var efi.var && echo "Deployed efi.var"
 cp /logo.bmp tools/logos/denx.bmp && cp /logo.bmp drivers/video/u_boot_logo.bmp
 sed -i 's/CONFIG_BAUDRATE=1500000/CONFIG_BAUDRATE=115200/' configs/rockpro64-rk3399_defconfig
 cat /rk3399_defconfig >> configs/rockpro64-rk3399_defconfig
+cat configs/rockpro64-rk3399_defconfig
 make rockpro64-rk3399_defconfig
 FORCE_SOURCE_DATE=1 SOURCE_DATE=$SOURCE_DATE SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH make -j $(nproc) all
 ls -la
@@ -22,6 +23,7 @@ cp /efi.var efi.var && echo "Deployed efi.var"
 cp /logo.bmp tools/logos/denx.bmp && cp /logo.bmp drivers/video/u_boot_logo.bmp
 sed -i 's/CONFIG_BAUDRATE=1500000/CONFIG_BAUDRATE=115200/' configs/pinebook-pro-rk3399_defconfig
 cat /rk3399_defconfig >> configs/pinebook-pro-rk3399_defconfig
+cat configs/pinebook-pro-rk3399_defconfig
 make pinebook-pro-rk3399_defconfig
 FORCE_SOURCE_DATE=1 SOURCE_DATE=$SOURCE_DATE SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH make -j $(nproc) all
 ls -la
