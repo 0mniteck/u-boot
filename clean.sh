671dfcfe137b19fac6b8d0271a8d3c18f0e596c0
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-05 05:57:54 -0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 if [ "$1" = "yes" ]; then
-pushd Builds/
-find . ! -type d -delete
-for loc in RP64-rk3399 PBP-rk3399 RP64-rk3399-SB PBP-rk3399-SB
-do
-touch $loc/tmp
-done
-popd
+  pushd Builds/
+  find . ! -type d -delete
+  for loc in RP64-rk3399 PBP-rk3399 RP64-rk3399-SB PBP-rk3399-SB
+  do
+    touch $loc/tmp
+  done
+  popd
+elseif [ "$1" = "cleanup" ];
+  for loc in RP64-rk3399 PBP-rk3399 RP64-rk3399-SB PBP-rk3399-SB
+  do
+    rm -f $loc/tmp
+  done
 fi
 exit
