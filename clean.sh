2013cb68f0a2f42170a551c66d48fdbae330fb49
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-06 18:47:37 -0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -7,10 +7,12 @@ if [ "$1" = "yes" ]; then
     touch $loc/tmp
   done
   popd
+  rm -f /tmp/builder.log
 elseif [ "$1" = "cleanup" ];
   for loc in RP64-rk3399 PBP-rk3399 RP64-rk3399-SB PBP-rk3399-SB
   do
     rm -f $loc/tmp
   done
+  rm -f /tmp/builder.log
 fi
 exit
