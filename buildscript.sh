c91f9f8272814fdafd6a6382a20797c2339f27cf
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-06 18:48:02 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -4,7 +4,7 @@ sudo snap install syft --classic
 git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
 ./clean.sh $1
 sudo screen -L -Logfile /tmp/builder.log bash -c './re-run.sh '$(($2))
-cp /tmp/builder.log Builds/builder.log && rm -f /tmp/builder.log
+cp /tmp/builder.log Builds/builder.log
 ./clean.sh cleanup
 ./git.sh "Build Artifact Added"
 ls -la Builds/*
