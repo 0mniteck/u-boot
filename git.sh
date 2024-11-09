b5f38eea077ebef0084a0b182db5f96b137cb176
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-09 11:51:55 -0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -2,3 +2,6 @@
 
 git status && git add -A && git status
 git commit -a -S -m "$1" && sleep 5 && git push --set-upstream origin rk3399-Docker
+if [ "$2" != "" ]; then
+  git tag -a "$2" -s -m "Tagged Release $2" && git push origin $2
+fi
