690babfba556abd219a8494eb8f798a3a6d27b5b
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-10 23:21:48 -0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -4,27 +4,27 @@ chmod +x Configs/dev-config.sh
 pushd Builds/
   if [ "$1" = "yes" ]; then
     find . ! -type d -delete
-    for dev in RP64-rk3399 PBP-rk3399 PT2-rk3566
+    for dev in RP64-rk3399 PBP-rk3399 PT2-rk3566 R5B-rk3588
     do
       for loc in $dev $dev-SB $dev-MU-SB
       do
         touch $loc/tmp
       done
     done
-    for arch in rk3399 rk3568
+    for arch in rk3399 rk3568 rk3588
     do
       touch $arch/tmp
     done
   fi
   if [ "$1" = "cleanup" ]; then
-    for dev in RP64-rk3399 PBP-rk3399 PT2-rk3566
+    for dev in RP64-rk3399 PBP-rk3399 PT2-rk3566 R5B-rk3588
     do
       for loc in $dev $dev-SB $dev-MU-SB
       do
         rm -f $loc/tmp
       done
     done
-    for arch in rk3399 rk3568
+    for arch in rk3399 rk3568 rk3588
     do
       rm -f $arch/tmp
     done
