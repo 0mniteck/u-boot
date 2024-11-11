ca4af85ceb516ce04afd6cca21a120025706220c
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-10 19:05:38 -0800

    Update arm-trusted-buildscript.sh

--- Buildscripts/arm-trusted-buildscript.sh
+++ Buildscripts/arm-trusted-buildscript.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 trap '[[ $pid ]] && kill $pid; exit' EXIT
 echo "SOURCE_DATE_EPOCH: $SOURCE_DATE_EPOCH"
-echo "Entering /rk3399/arm-trusted-firmware-lts-v$ATF_VER"
-pushd /rk3399/arm-trusted-firmware-lts-v$ATF_VER
-make realclean && make BUILD_MESSAGE_TIMESTAMP="$(echo '"'$BUILD_MESSAGE_TIMESTAMP'"')" PLAT=rk3399 bl31
-ls -la build/rk3399/release/bl31/
+echo "BUILD_MESSAGE_TIMESTAMP: $BUILD_MESSAGE_TIMESTAMP"
+for plat in rk3399 rk3568
+do
+echo "Entering /$plat/arm-trusted-firmware-lts-v$ATF_VER"
+pushd /$plat/arm-trusted-firmware-lts-v$ATF_VER
+make realclean && make BUILD_MESSAGE_TIMESTAMP="$(echo '"'$BUILD_MESSAGE_TIMESTAMP'"')" PLAT=$plat bl31
+ls -la build/$plat/release/bl31/
 popd
-echo "Entering /rk3568/arm-trusted-firmware-lts-v$ATF_VER"
-pushd /rk3568/arm-trusted-firmware-lts-v$ATF_VER
-make realclean && make BUILD_MESSAGE_TIMESTAMP="$(echo '"'$BUILD_MESSAGE_TIMESTAMP'"')" PLAT=rk3568 bl31
-ls -la build/rk3568/release/bl31/
-popd
+done
