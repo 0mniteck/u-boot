87d0de41e9645bbc15a44ec10b15459a6945ab4b
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-10 17:11:52 -0800

    Update arm-trusted-buildscript.sh

--- Buildscripts/arm-trusted-buildscript.sh
+++ Buildscripts/arm-trusted-buildscript.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 trap '[[ $pid ]] && kill $pid; exit' EXIT
 echo "SOURCE_DATE_EPOCH: $SOURCE_DATE_EPOCH"
-echo "Entering /arm-trusted-firmware-lts-v$ATF_VER"
-pushd /arm-trusted-firmware-lts-v$ATF_VER
+echo "Entering /rk3399/arm-trusted-firmware-lts-v$ATF_VER"
+pushd /rk3399/arm-trusted-firmware-lts-v$ATF_VER
 make realclean && make BUILD_MESSAGE_TIMESTAMP="$(echo '"'$BUILD_MESSAGE_TIMESTAMP'"')" PLAT=rk3399 bl31
-ls -la /arm-trusted-firmware-lts-v$ATF_VER/build/rk3399/release/bl31/
+ls -la build/rk3399/release/bl31/
+popd
+echo "Entering /rk3568/arm-trusted-firmware-lts-v$ATF_VER"
+pushd /rk3568/arm-trusted-firmware-lts-v$ATF_VER
+make realclean && make BUILD_MESSAGE_TIMESTAMP="$(echo '"'$BUILD_MESSAGE_TIMESTAMP'"')" PLAT=rk3568 bl31
+ls -la build/rk3568/release/bl31/
 popd
