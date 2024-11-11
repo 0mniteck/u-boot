f60652a56e08feb6d1c63e192a087aadedec0c46
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-10 18:51:48 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -4,7 +4,8 @@ git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
 ./clean.sh $1
 sudo screen -L -Logfile /tmp/builder.log bash -c './re-run.sh '$(($2))
 cp /tmp/builder.log Builds/builder.log
+status="$(cat /tmp/status.build)"
 ./clean.sh cleanup
-read -p "$(cat /tmp/status.build): --> sign and commit"
-./git.sh "$(cat /tmp/status.build)" "$3"
-rm -f /tmp/status.build && ls -la Builds/* && cd ..
+read -p "$status: --> sign, commit, and push"
+./git.sh "$status" "$3"
+ls -la Builds/* && cd ..
