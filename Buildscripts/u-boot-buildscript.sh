e04b20292b528b5200b3c6497cdc221056ef0d17
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-11 09:36:16 -0800

    Update u-boot-buildscript.sh

--- Buildscripts/u-boot-buildscript.sh
+++ Buildscripts/u-boot-buildscript.sh
@@ -10,10 +10,10 @@ for dev in RP64-rk3399:rockpro64-rk3399_defconfig PBP-rk3399:pinebook-pro-rk3399
     echo "Entering /$(echo $loc | cut -d':' -f1)/u-boot-$UB_VER"
     pushd /$(echo $loc | cut -d':' -f1)/u-boot-$UB_VER
       make clean
-      ./$(echo $loc | cut -d':' -f2)config.sh
+      ./../../$(echo $loc | cut -d':' -f2)config.sh
       cp /efi.var efi.var && echo "Deployed efi.var"
       cp /logo.bmp tools/logos/denx.bmp && cp /logo.bmp drivers/video/u_boot_logo.bmp
-      if ["$(echo $dev | cut -d':' -f2)" = "pinebook-pro-rk3399_defconfig"]; then
+      if [ "$(echo $dev | cut -d':' -f2)" = "pinebook-pro-rk3399_defconfig" ]; then
         cp /rk3399-pinebook-pro-u-boot.dtsi arch/arm/dts/rk3399-pinebook-pro-u-boot.dtsi && echo "Patched Device Tree bug"
       fi
       sed -i 's/CONFIG_BAUDRATE=1500000/CONFIG_BAUDRATE=115200/' configs/$(echo $dev | cut -d':' -f2)
