228dc7a5fbfcdadadc36d335f6c5e23580f1f935
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-11 20:46:07 -0800

    Update optee-buildscript.sh

--- Buildscripts/optee-buildscript.sh
+++ Buildscripts/optee-buildscript.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 trap '[[ $pid ]] && kill $pid; exit' EXIT
 echo "SOURCE_DATE_EPOCH: $SOURCE_DATE_EPOCH"
+RUN unzip -q $OPT_VER.zip
 echo "Entering /optee_os-$OPT_VER"
 pushd /optee_os-$OPT_VER
 make -j $(nproc) PLATFORM=rockchip-rk3399 CFG_ARM64_core=y CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE32=arm-linux-gnueabihf- CROSS_COMPILE_core=aarch64-linux-gnu- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu-
