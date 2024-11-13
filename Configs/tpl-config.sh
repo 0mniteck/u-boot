0aaa1d12b8321c4d319290ceb31af7d5b8a383b4
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-12 22:24:51 -0800

    Update tpl-config.sh

--- Configs/tpl-config.sh
+++ Configs/tpl-config.sh
@@ -3,7 +3,7 @@
 echo "CONFIG_TPL=y" >> defconfig
 echo "CONFIG_SUPPORT_TPL=y" >> defconfig
 echo "CONFIG_TPL_NEEDS_SEPARATE_STACK=y" >> defconfig
-echo "CONFIG_ROCKCHIP_EXTERNAL_TPL=n
+echo "CONFIG_ROCKCHIP_EXTERNAL_TPL=n" >> defconfig
 echo "CONFIG_RAM_ROCKCHIP_LPDDR4=y" >> defconfig
 echo "CONFIG_TPL_DM=y" >> defconfig
 echo "CONFIG_TPL_LIBCOMMON_SUPPORT=y" >> defconfig
