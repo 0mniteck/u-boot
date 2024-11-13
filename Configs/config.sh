9804f2d9fce7dcefeddcbc8d9cdd963295fede4f
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-13 03:16:38 -0800

    Update config.sh

--- Configs/config.sh
+++ Configs/config.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo 'CONFIG_SYS_PROMPT="0MNITECK:~$ "' >> defconfig
+echo 'CONFIG_LOCALVERSION=" 0MNITECK"' >> defconfig
 echo "CONFIG_FIT_SIGNATURE=y" >> defconfig
 echo "CONFIG_TEE=y" >> defconfig
 echo "CONFIG_OPTEE=y" >> defconfig
@@ -9,5 +11,3 @@ echo "CONFIG_BOOTM_OPTEE=y" >> defconfig
 echo "CONFIG_OPTEE_TA_SCP03=n" >> defconfig
 echo "CONFIG_OPTEE_TA_AVB=n" >> defconfig
 echo "CONFIG_CHIMP_OPTEE=n" >> defconfig
-echo 'CONFIG_SYS_PROMPT="0MNITECK:~$ "' >> defconfig
-echo 'CONFIG_LOCALVERSION=" 0MNITECK"' >> defconfig
