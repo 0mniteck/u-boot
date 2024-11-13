c64f1f0b56359f79e4979ac09e4fb2df3efddce6
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-12 23:22:03 -0800

    Update u-boot-buildscript.sh

--- Buildscripts/u-boot-buildscript.sh
+++ Buildscripts/u-boot-buildscript.sh
@@ -12,17 +12,17 @@ for dev in RP64-rk3399:rockpro64-rk3399_defconfig PBP-rk3399:pinebook-pro-rk3399
       make clean
       ./../../$(echo $loc | cut -d':' -f2)config.sh
       cp /efi.var efi.var && echo "Deployed efi.var"
-      cp /logo.bmp tools/logos/denx.bmp && cp /logo.bmp drivers/video/u_boot_logo.bmp
+      cp /logo.bmp tools/logos/denx.bmp && cp /logo.bmp drivers/video/u_boot_logo.bmp && echo "Deployed Logo"
       if [ "$(echo $dev | cut -d':' -f2)" = "pinebook-pro-rk3399_defconfig" ]; then
-        cp /rk3399-pinebook-pro-u-boot.dtsi arch/arm/dts/rk3399-pinebook-pro-u-boot.dtsi && echo "Patched Device Tree bug"
+        cp /rk3399-pinebook-pro-u-boot.dtsi arch/arm/dts/rk3399-pinebook-pro-u-boot.dtsi && echo "Patched Device Tree Bug"
       fi
       if [ "$(echo $dev | cut -d':' -f2)" = "pinetab2-rk3566_defconfig" ] || \
          [ "$(echo $dev | cut -d':' -f2)" = "rock5b-rk3588_defconfig" ]; then
         ./../../tpl-config.sh
-        sed -i '112,117d' arch/arm/mach-rockchip/sdram.c
+        sed -i '112,117d' arch/arm/mach-rockchip/sdram.c && echo "Deployed Rockchip TPL Bypass"
       fi
       sed -i 's/CONFIG_BAUDRATE=1500000/CONFIG_BAUDRATE=115200/' configs/$(echo $dev | cut -d':' -f2)
-      cat defconfig >> configs/$(echo $dev | cut -d':' -f2)
+      cat defconfig >> configs/$(echo $dev | cut -d':' -f2) && echo "Appended Defconfig"
       cat configs/$(echo $dev | cut -d':' -f2)
       make $(echo $dev | cut -d':' -f2)
       platt=$(echo $(echo $dev | cut -d':' -f1) | cut -d'-' -f2)
