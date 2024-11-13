556bbae4c96f017ecde83d1c39b1aa9c9e23ee8b
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-13 03:11:03 -0800

    Update sb-config.sh

--- Configs/sb-config.sh
+++ Configs/sb-config.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "CONFIG_CMD_BOOTEFI=y" >> defconfig
+echo 'CONFIG_SYS_PROMPT="0MNITECK:~$ "' >> defconfig
+echo 'CONFIG_LOCALVERSION=" 0MNITECK"' >> defconfig
 echo "CONFIG_FIT_SIGNATURE=y" >> defconfig
 echo "CONFIG_RSA=y" >> defconfig
 echo "CONFIG_ECDSA=y" >> defconfig
@@ -12,11 +15,8 @@ echo "CONFIG_BOOTM_OPTEE=y" >> defconfig
 echo "CONFIG_OPTEE_TA_SCP03=n" >> defconfig
 echo "CONFIG_OPTEE_TA_AVB=n" >> defconfig
 echo "CONFIG_CHIMP_OPTEE=n" >> defconfig
+echo "CONFIG_EFI_SECURE_BOOT=y" >> defconfig
+echo "CONFIG_EFI_LOADER=y" >> defconfig
 echo "CONFIG_EFI_VARIABLE_NO_STORE=y" >> defconfig
 echo "CONFIG_EFI_VARIABLES_PRESEED=y" >> defconfig
 echo 'CONFIG_EFI_VAR_SEED_FILE="efi.var"' >> defconfig
-echo "CONFIG_EFI_SECURE_BOOT=y" >> defconfig
-echo "CONFIG_EFI_LOADER=y" >> defconfig
-echo "CONFIG_CMD_BOOTEFI=y" >> defconfig
-echo 'CONFIG_SYS_PROMPT="0MNITECK:~$ "' >> defconfig
-echo 'CONFIG_LOCALVERSION=" 0MNITECK"' >> defconfig
