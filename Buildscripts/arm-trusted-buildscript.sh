81128b8dc82d467ac1c7c03bf956a3b8a0b1ee1d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-13 06:31:27 -0800

    Update arm-trusted-buildscript.sh

--- Buildscripts/arm-trusted-buildscript.sh
+++ Buildscripts/arm-trusted-buildscript.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 trap '[[ $pid ]] && kill $pid; exit' EXIT
 echo "SOURCE_DATE_EPOCH: $SOURCE_DATE_EPOCH" && echo "BUILD_MESSAGE_TIMESTAMP: $BUILD_MESSAGE_TIMESTAMP"
-ARCHS="rk3399 rk3568 rk3588"
 for plat in $ARCHS
 do
   unzip -q $ATF_VER.zip -d /$plat
