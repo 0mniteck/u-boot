87dc94711df9dca4c2af999f29d5bab6117e4c9b
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-13 04:11:38 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -2,7 +2,7 @@
 sudo apt install -y bc dosfstools parted screen snapd && sudo snap install syft --classic
 git remote remove origin && git remote add origin git@UBoot:0mniteck/U-Boot.git
 ./clean.sh $1
-sudo screen -L -Logfile builder.log bash -c './re-run.sh '$(($2))
+sudo screen -L -Logfile builder.log bash -c './re-run.sh '$(($2))' '$4
 mv builder.log Builds/builder.log
 status="$(cat status.build)"
 ./clean.sh cleanup
