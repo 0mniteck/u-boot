4a92ffe0a3f8ced3b9dceb4f5c27fb5317c9b18e
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-12 22:16:39 -0800

    Create tpl-config.sh

---
+++ Configs/tpl-config.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "CONFIG_TPL=y" >> defconfig
+echo "CONFIG_SUPPORT_TPL=y" >> defconfig
+echo "CONFIG_TPL_NEEDS_SEPARATE_STACK=y" >> defconfig
+echo "CONFIG_ROCKCHIP_EXTERNAL_TPL=n
+echo "CONFIG_RAM_ROCKCHIP_LPDDR4=y" >> defconfig
+echo "CONFIG_TPL_DM=y" >> defconfig
+echo "CONFIG_TPL_LIBCOMMON_SUPPORT=y" >> defconfig
+echo "CONFIG_TPL_LIBGENERIC_SUPPORT=y" >> defconfig
+echo "CONFIG_TPL_ROCKCHIP_COMMON_BOARD=y" >> defconfig
+echo "CONFIG_TPL_SERIAL=y" >> defconfig
