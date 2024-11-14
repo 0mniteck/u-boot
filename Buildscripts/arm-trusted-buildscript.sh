963bfd1d48ccc1d49ecf9827da5bd42ba868310e
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-14 04:01:21 -0800

    Update arm-trusted-buildscript.sh

--- Buildscripts/arm-trusted-buildscript.sh
+++ Buildscripts/arm-trusted-buildscript.sh
@@ -3,7 +3,7 @@ trap '[[ $pid ]] && kill $pid; exit' EXIT
 for plat in $ARCHS
 do
   echo "Unzipping TF-A for $plat..."
-  unzip -q $ATF_VER.zip -d /$plat > /dev/null
+  unzip -q v$ATF_VER.zip -d /$plat > /dev/null
   echo "Entering /$plat/arm-trusted-firmware-$ATF_VER"
   pushd /$plat/arm-trusted-firmware-$ATF_VER
     make realclean && make BUILD_MESSAGE_TIMESTAMP="$(echo '"'$BUILD_MESSAGE_TIMESTAMP'"')" PLAT=$plat bl31
