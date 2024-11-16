a5d37046c52efdf3bf9221ea6bc217797f79481b
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-15 20:24:27 -0800

    Update xpl-config.sh

--- Configs/xpl-config.sh
+++ Configs/xpl-config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#echo "CONFIG_TPL=y" >> defconfig
+echo "CONFIG_TPL=y" >> defconfig
 #echo "CONFIG_TPL_DM=y" >> defconfig
 #echo "CONFIG_TPL_SERIAL=y" >> defconfig
 #echo "CONFIG_SUPPORT_TPL=y" >> defconfig
