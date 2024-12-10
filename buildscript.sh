cab9f5b77529dc7c680b8a8d6737171bfc0dc026
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-12-10 14:54:28 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 export HUB="0mniteck/debian"
-export BASE="11-11-2024@sha256:bec06f3f431ddb41a879956d00caacad6ab23a899fc495ec6d27af5f490e6b43"
-export BASE_EXTRA="11-11-2024@sha256:fc26a6d3e47e65d63b62f91840ac8b151dff871df8d68093ec2329654ada0d2d"
+export BASE="12-2-2024@sha256:2746563d94f92faac307da344c0ba430cdb4176128c70c0f10d3a8b71cb3946d"
+export BASE_EXTRA="12-2-2024@sha256:f8b95aa60064bedfb77f580144cbfca5d42052ff7eb237e79cb7eb0c4fb669e4"
 
 export OPT_VER="4.4.0"
 export OPT_SUM="2fae73356770a0eb6e519a8b9ef32e566dd900778e3b52ccb79a63d767cc9dfaa52b920ee94955ef32bbe30304636dc6c26d3f2615483bdd8d4d1d76cdfdaed9"
-export ATF_VER="2.12-rc0"
-export ATF_SUM="c9c2c44cf8a412c228fcce87949ce155500743e7f39bed505e7416058738ab9c67afbcb447f9f6706d7e57779485b1d811648c1636fdd1eae0d7d0da7808ee17"
-export UB_VER="2025.01-rc2"
-export UB_SUM="0ba126110942f1d5bb3dec3f0f17fab0fa13fce9f4e532e4387167e8e1325ee4b63a17accdbeb102dd2a6fbe70a48dd488953169919da8a4c5d052b250df464d"
+export ATF_VER="2.12.0"
+export ATF_SUM="3940d565ec2d7918a6e943261fff2a05d9886dea46a15c966f691a07ed20a31d45c5c154f12d437db34bf229dc80d697632155dc11dcb52a9e09568fad2bb655"
+export UB_VER="2025.01-rc4"
+export UB_SUM="53751f9feeff1fd9a6780f6bfc5034d77c9bdd256dde59cdc3c38603521e453fb834683f57473c5cbcc86513cbe9d9ee9eeeecfeefdf41a8c8e721df0e06f1bd"
 
 export BUILD_LIST="PT2-rk3566:pinetab2-rk3566_defconfig RP64-rk3399:rockpro64-rk3399_defconfig PBP-rk3399:pinebook-pro-rk3399_defconfig R5B-rk3588:rock5b-rk3588_defconfig"
 export LIST="PT2-rk3566 RP64-rk3399 PBP-rk3399 R5B-rk3588"
